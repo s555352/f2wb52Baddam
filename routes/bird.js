a667fe913d117b91c1b09c8e76d96e61d1a3aec8
Asritha Baddam <s555352@nwmissouri.edu>
2022-11-02 17:20:46 -0500

    modified bird code

--- routes/bird.js
+++ routes/bird.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('bird', { title: 'Search Results Bird' });
+  res.render('Bird', { title: 'Search Results Bird' });
 });
 
 module.exports = router;
